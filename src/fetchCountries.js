1e7cb6888a8f5fe0a86e8536e8c040befb806e03
YehorAntoniuk <antonukdeys@gmail.com>
2022-09-28 13:34:31 +0300

    fix 2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,17 +1,6 @@
-// export const fetchCountries = function (country) {
-//   const filter = 'fields=name,capital,population,flags,languages';
-
-//   return fetch(`https://restcountries.com/v3.1/name/${country}?${filter}`).then(
-//     response => {
-//       if (response.ok) {
-//         return response.json();
-//       }
-//     }
-//   );
-// };
-
 export const fetchCountries = function (country) {
-  let filter = 'fields=name,capital,population,flags,languages';
+  const filter = 'fields=name,capital,population,flags,languages';
+
   return fetch(`https://restcountries.com/v3.1/name/${country}?${filter}`).then(
     response => {
       if (response.ok) {
